4f8ff4eba403e698b14f803d7047ea1401481ff3
Ray Crawford <ray_crawford@trekbikes.com>
2017-01-10 21:44:41 -0600

    Initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
 name 'cb_dvo_osUpdate'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Ray Crawford'
+maintainer_email 'ray_crawford@trekbikes.com'
 license 'all_rights'
 description 'Installs/Configures cb_dvo_osUpdate'
 long_description 'Installs/Configures cb_dvo_osUpdate'
+issues_url 'https://bitbucket.org/trekbikes/cb_dvo_adJoin/issues'
+source_url 'https://bitbucket.org/trekbikes/cb_dvo_adJoin/src'
 version '0.1.0'
 
 # If you upload to Supermarket you should set this so your cookbook
