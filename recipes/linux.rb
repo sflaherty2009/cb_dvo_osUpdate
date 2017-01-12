bf9acc3fc7b610240cda5ab58508b6fc660631e4
Ray Crawford <ray_crawford@trekbikes.com>
2017-01-11 22:21:02 -0600

    refactored

--- recipes/linux.rb
+++ recipes/linux.rb
@@ -4,6 +4,16 @@
 #
 # Copyright (c) 2017 Trek Bicycles, All Rights Reserved.
 
-execute 'update' do
+cookbook_file '/etc/cron.monthly/yumUpdate.bash' do
+  source '/etc/cron.monthly/yumUpdate.bash'
+  action :create
+  owner 'root'
+  group 'root'
+  mode '0700'
+  notifies :run, 'execute[yum Update]', :immediately
+end
+
+# Guarded so it only runs the first time
+execute 'yum Update' do
   command 'yum update -y'
 end
