16360d6b699000ec4556c6e0597cb4781132eb77
Ray Crawford <ray_crawford@trekbikes.com>
2017-05-10 00:32:15 -0400

    Added docs, added environments and passed linting

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# 0.1.1
+**Date: 5/8/2017**
+
+* Updated metadata.rb to pass updated foodcritic linting
+* Added CHANGELOG.md
+* Added environments.json
+
+# 0.1.0
+Date: 1/10/2017
+
+* Initial commit
--- metadata.rb
+++ metadata.rb
@@ -8,10 +8,5 @@ issues_url 'https://bitbucket.org/trekbikes/cb_dvo_adJoin/issues'
 source_url 'https://bitbucket.org/trekbikes/cb_dvo_adJoin/src'
 version '0.1.0'
 
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/cb_dvo_osUpdate/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/cb_dvo_osUpdate' if respond_to?(:source_url)
+chef_version '~> 12'
+supports 'centos'
