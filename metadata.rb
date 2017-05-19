b941863e74dfcb501a090bb749d69c216305a2a2
Ray Crawford <ray_crawford@trekbikes.com>
2017-05-18 23:24:58 -0400

    Fix chef_version

--- metadata.rb
+++ metadata.rb
@@ -8,5 +8,5 @@ issues_url 'https://bitbucket.org/trekbikes/cb_dvo_adJoin/issues'
 source_url 'https://bitbucket.org/trekbikes/cb_dvo_adJoin/src'
 version '0.1.2'
 
-chef_version '~> 12'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'centos'
