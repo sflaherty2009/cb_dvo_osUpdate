49d4ae6a247da66175daca9cbdc26092c27c240f
Ray Crawford <ray_crawford@trekbikes.com>
2017-05-18 23:26:07 -0400

    inc ver

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures cb_dvo_osUpdate'
 long_description 'Installs/Configures cb_dvo_osUpdate'
 issues_url 'https://bitbucket.org/trekbikes/cb_dvo_adJoin/issues'
 source_url 'https://bitbucket.org/trekbikes/cb_dvo_adJoin/src'
-version '0.1.2'
+version '0.1.3'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'centos'
